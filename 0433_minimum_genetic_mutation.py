b69e14801428dc448fd13b618fd524d651aedc7a
Pablo Oliver Cortés <olivercortespablo@gmail.com>
2022-11-02 16:02:50 +0100

    Daily 02/11

--- 0433_minimum_genetic_mutation.py
+++ 0433_minimum_genetic_mutation.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+#
+# https://leetcode.com/problems/minimum-genetic-mutation/
+
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        from collections import deque
+
+        previous = {start}
+        start = deque([[start, 0]])
+        mutations = -1
+
+        while start:
+            tmp, mutations = start.popleft()
+
+            if tmp == end:
+                return mutations
+
+            for gene in bank:
+                # Check gene mutation count
+                dist = 0
+                for i in range(8):
+                    if tmp[i] != gene[i]:
+                        dist += 1
+
+                if dist == 1 and gene not in previous:
+                    previous.add(gene)
+                    start.append([gene, mutations + 1])
+
+        return -1
